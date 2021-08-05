428de7e9c3094546aa4305d07eabe9ffb86decd0
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-08-05 16:42:48 -0300

    Created edit and delete buttons with initial behavior

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, Text, FlatList, StyleSheet, Alert } from 'react-native';
+import { Button, ListItem } from 'react-native-elements';
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
 
 import users from '../data/users';
 
 const UserList = (props) => {
-  const getUserItem = ({ item }) => {
+  const confirmUserDeletion = (user) => {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn(`Deletado ${user.name}`);
+        }
+      },
+      {
+        text: 'Não'
+      }
+    ]);
+  };
+
+  const getAction = (user) => {
+    return (
+      <>
+        <Button
+          onPress={ () => props.navigation.navigate('UserForm', user) }
+          type="clear"
+          icon={
+            <Icon
+              name="edit"
+              size={ 25 }
+              color="orange"
+            />
+          }
+        />
+
+        <Button
+          onPress={ () => confirmUserDeletion(user) }
+          type="clear"
+          icon={
+            <Icon
+              name="delete"
+              size={ 25 }
+              color="red"
+            />
+          }
+        />
+
+      </>
+    );
+  };
+
+  const getUserItem = ({ item: user }) => {
     return (
       <ListItem bottomDivider
-        onPress={ () => props.navigation.navigate('UserForm') }
+        onPress={ () => props.navigation.navigate('UserForm', user) }
       >
-        <Avatar source={ { uri: item.avatarUrl } } />
+        <Avatar source={ { uri: user.avatarUrl } } />
         <ListItem.Content>
-          <ListItem.Title>{ item.name }</ListItem.Title>
-          <ListItem.Subtitle>{ item.email }</ListItem.Subtitle>
+          <ListItem.Title>{ user.name }</ListItem.Title>
+          <ListItem.Subtitle>{ user.email }</ListItem.Subtitle>
         </ListItem.Content>
+        <View style={ styles.userIcons }>
+          {
+            getAction(user)
+          }
+        </View>
       </ListItem>
     );
   };
@@ -30,5 +81,11 @@ const UserList = (props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  userIcons: {
+    flexDirection: 'row'
+  }
+});
 
 export { UserList };
