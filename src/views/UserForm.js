b8ac25070820ab4942701b683fbcbdc8b8e68e98
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-08-06 11:58:53 -0300

    Created edit user screen

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
-const UserForm = () => {
+const UserForm = ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
   return (
-    <Text>
-      UserForm
-    </Text>
+    <View style={ styles.form }>
+      <Text>Nome</Text>
+      <TextInput
+        onChangeText={ name => setUser({ ...user, name }) }
+        placeholder="Informe o nome"
+        value={ user.name }
+        style={ styles.input }
+      />
+      <Text>Email</Text>
+      <TextInput
+        onChangeText={ email => setUser({ ...user, email }) }
+        placeholder="Informe o e-mail"
+        value={ user.email }
+        style={ styles.input }
+      />
+      <Text>URL do avatar</Text>
+      <TextInput
+        onChangeText={ avatarUrl => setUser({ ...user, avatarUrl }) }
+        placeholder="Informe a URL do avatar"
+        value={ user.avatarUrl }
+        style={ styles.input }
+      />
+      <Button
+        title="Salvar"
+        onPress={ () => {
+          navigation.goBack();
+        } }
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 12
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+});
 
 export { UserForm };
 
